c9406a5a17c96af5fef62695e2572cc3c3671aa9
ilakosterin <ilya@in10.top>
2021-07-11 14:23:19 +0300

    mylessons1

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+//RegExp поиск платьев с ценой в описании товара
+/*let products = [];
+let hasDigitalsDescription = products.filter(element => /(?!2015|2016|2019|2020)\d{1,} ?\d{3,}(?!px|em|rem)/.test(element.description));
+
+console.log(hasDigitalsDescription);*/
+var arr = [0.1,2.1,3,0,4,1,6];
+
+alert(arr.indexOf(0));
+
+
